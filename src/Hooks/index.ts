d480611969820f1f904fb4a4ef592e2964b608aa
luizantoniocardoso <la2004.o.sc@gmail.com>
2024-05-03 21:28:08 -0300

    feat: :sparkles: criando hook de fetch

---
+++ src/Hooks/index.ts
@@ -0,0 +1,7 @@
+
+import { useFetch } from './useFetch';
+
+export {
+    useFetch,
+    
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,29 @@
 import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from "react"
-import { LoginCard } from "../../components/LoginCard"
-import { Forms } from "../../components/Forms";
-import { useNavigate } from "react-router-dom";
-
-
+import { useNavigate } from "react-router-dom"
+import { LoginCard } from "@/components/LoginCard"
+import { Forms } from "@/components/Forms"
+import { useFetch } from "@/Hooks/useFetch"
 
 
 export const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [password, setPassword] = useState("");
-    const [userName, setUserName] = useState("");
-    const [error, setError] = useState(false);
-
+    const [email, setEmail] = useState("");
+    const [errorInput, setErrorInput] = useState(false);
+    
     const navigate = useNavigate();
+    const {} = useFetch();
 
 
-    const refUserName = useRef<HTMLInputElement>(null);
+    const refEmail = useRef<HTMLInputElement>(null);
     const refPassword = useRef<HTMLInputElement>(null);
 
-    const handleUserNameChange = (e: ChangeEvent<HTMLInputElement>) => setUserName(e.target.value)
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
     const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
     
     useEffect(() => {
-        setError(false)
-    }, [password, userName])
+        setErrorInput(false)
+    }, [password, email])
 
     const handleCheckBoxChange = () => {
         setShowPassword(!showPassword)
@@ -35,24 +35,20 @@ export const Login = () => {
 
     const handleLogin = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        const isCorrectLogin = userName === "admin" && password === "admin";
-        if(isCorrectLogin) navigate("/home")
-        if(!isCorrectLogin)setError(true)
-
+        fetch.get("https://jsonplaceholder.typicode.com/posts" )
+        navigate("/home")
     }
-
-    
 
     return(
         <section className="flex items-center justify-center w-full h-screen bg-slate-200" >
             <LoginCard.Root>
                 <LoginCard.Img src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" alt="Imagem de login" />
                 <Forms.Root>
-                    <Forms.Input id="userNameInput" arialabel="Nome do usuario" type="text" placeholder="Nome do usuario" ref={refUserName} onChangeAction={handleUserNameChange} value={userName}>
-                     {error && <Forms.Small id="userNameInput" text="Verifique se o usuario esta correto" />}
+                    <Forms.Input id="emailInput" arialabel="Email" type="text" placeholder="Email" ref={refEmail} onChangeAction={handleEmailChange} value={email}>
+                     {errorInput && <Forms.Small id="emailInput" text="Verifique se o Email esta correto" />}
                     </Forms.Input>
                     <Forms.Input id="passwordInput" arialabel="Senha" type="password" placeholder="Senha" ref={refPassword} onChangeAction={handlePasswordChange} value={password}>
-                     {error && <Forms.Small id="passwordInput" text="Verifique se a senha esta correta" />}
+                     {errorInput && <Forms.Small id="passwordInp ut" text="Verifique se a senha esta correta" />}
                     </Forms.Input>
                     <Forms.CheckBox id="senha" content="Mostrar a senha" action={handleCheckBoxChange} checked={showPassword}>
                         <Forms.Link href="/home" text="Esqueceu a senha?"/>
